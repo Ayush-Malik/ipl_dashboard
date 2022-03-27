40f76f5b23a57610a7dd42c1f95a25e7308e2df7
Abhay_Dhiman <abhaysdhimans@gmail.com>
2022-03-27 11:50:12 +0530

    Added Season wise plots

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from functions import *
+from functions import Cleaner, winningPercentage, team_in_most_season, player_match_season, win_visu_by_toss
+
+# Basic Initialization
+cleaner_obj = Cleaner()
+cleaner_obj.clean_inconsistent_teams()
+cleaner_obj.replace_team_shortforms()
+
 
 dic = {'Sunrisers Hyderabad': 'SRH', 'Kolkata Knight Riders': 'KKR',
        'Royal Challengers Bangalore': 'RCB', 'Kings XI Punjab': 'KXIP',
@@ -19,9 +23,11 @@ st.text("")
 
 # Sidebar
 st.sidebar.markdown("### Parameters")
+season_labels = [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
 
 team1 = st.sidebar.selectbox("Team-1", dic.keys())
 team2 = st.sidebar.selectbox("Team-2", dic.keys(), index=1)
+season = st.sidebar.multiselect("Seasons", season_labels, season_labels)
 
 container1 = st.container()
 col1, col2 = st.columns(2)
@@ -32,7 +38,7 @@ with container1:
               # Pie Plot
               st.markdown("<p style='text-align: center;'>Winning percentage between both teams</p>",
                      unsafe_allow_html=True)
-              st.plotly_chart(winningPercentage(dic[team1], dic[team2]), use_container_width=True)
+              st.plotly_chart(winningPercentage(dic[team1], dic[team2], cleaner_obj, season), use_container_width=True)
               st.text("")
               st.text("")
        
@@ -41,7 +47,7 @@ with container1:
               # histogram plot
               st.markdown("<p style='text-align: center;'>Match Count in each Season</p>",
                      unsafe_allow_html=True)
-              st.plotly_chart(team_in_most_season(dic[team1], dic[team2]), use_container_width=True)
+              st.plotly_chart(team_in_most_season(dic[team1], dic[team2], cleaner_obj, season), use_container_width=True)
               st.text("")
               st.text("")
 
@@ -49,7 +55,7 @@ with container1:
 # histogram
 st.markdown("<p style='text-align: center;'>Most player of the matches</p>",
             unsafe_allow_html=True)
-st.plotly_chart(player_match_season(dic[team1], dic[team2]), use_container_width=True)
+st.plotly_chart(player_match_season(dic[team1], dic[team2], cleaner_obj, season), use_container_width=True)
 st.text("")
 st.text("")
 
@@ -61,9 +67,9 @@ col3, col4 = st.columns(2)
 
 with container2:
        with col3:
-              st.plotly_chart(win_visu_by_toss(dic[team1]), use_container_width=True)
+              st.plotly_chart(win_visu_by_toss(dic[team1], cleaner_obj, season), use_container_width=True)
        with col4:
-              st.plotly_chart(win_visu_by_toss(dic[team2]), use_container_width=True)
+              st.plotly_chart(win_visu_by_toss(dic[team2], cleaner_obj, season), use_container_width=True)
 
 st.text("")
 st.text("")
--- functions.py
+++ functions.py
@@ -1,104 +1,148 @@
 import pandas as pd
 import plotly.express as px
-import pandas as pd
-import numpy as np
 
 color_ls = ["#e056fd", "#434343", "#e84393", "#5e5368"]
+season_ls = [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
 
-matches = pd.read_csv(r'E:\5th_sem_proj\ipl_dashboard\matches.csv')
+# --------------------------
+#       Section - 1
+#  -------------------------
+## Cleaning and manipulation of data 
+class Cleaner:
+    new_df = None
+    old_df = None
+    
+    def __init__(self) -> None:
+        matches = pd.read_csv(r'E:\5th_sem_proj\ipl_dashboard\matches.csv')
+        matches.replace(to_replace=['Delhi Daredevils'], value=['Delhi Capitals'], inplace=True)
+        
+        self.old_df = matches
+        self.new_df = matches
 
-matches.replace(to_replace=['Delhi Daredevils'], value=['Delhi Capitals'], inplace=True)
+    def get_teams(self):
+        consistent_teams = ['Sunrisers Hyderabad', 'Mumbai Indians',
+                            'Kolkata Knight Riders', 'Royal Challengers Bangalore',
+                            'Delhi Capitals', 'Kings XI Punjab', 'Chennai Super Kings', 'Rajasthan Royals']
+        return consistent_teams
 
-consistent_teams = ['Sunrisers Hyderabad', 'Mumbai Indians',
-                    'Kolkata Knight Riders', 'Royal Challengers Bangalore',
-                    'Delhi Capitals', 'Kings XI Punjab', 'Chennai Super Kings', 'Rajasthan Royals']
+    def get_short_team_name(self):
+        names = {'Sunrisers Hyderabad': 'SRH', 'Kolkata Knight Riders': 'KKR',
+        'Royal Challengers Bangalore': 'RCB', 'Kings XI Punjab': 'KXIP',
+        'Mumbai Indians': 'MI', 'Chennai Super Kings': 'CSK',
+        'Rajasthan Royals': 'RR', 'Delhi Capitals': 'DC'}
+        return names
 
+    def clean_inconsistent_teams(self):
+        # Taking data of only consistent teams and storing it into new dataframe
+        self.new_df = self.old_df[(self.old_df.team1.isin(self.get_teams())) & (self.old_df.team2.isin(self.get_teams()))]
 
-# Taking data of only consistent teams
-matches_2 = matches[(matches.team1.isin(consistent_teams))
-                    & (matches.team2.isin(consistent_teams))]
+    def replace_team_shortforms(self):
+        # Replacing names of teams to their short names
+        names = self.get_short_team_name()
+        self.new_df.replace(names, inplace=True)
+        self.old_df.replace(names, inplace=True)
 
-dic = {'Sunrisers Hyderabad': 'SRH', 'Kolkata Knight Riders': 'KKR',
-       'Royal Challengers Bangalore': 'RCB', 'Kings XI Punjab': 'KXIP',
-       'Mumbai Indians': 'MI', 'Chennai Super Kings': 'CSK',
-       'Rajasthan Royals': 'RR', 'Delhi Capitals': 'DC'
-       }
+# -------------------------------
+#          Section - 2
+# -------------------------------
+## Different Stats and data manipulation for Plots
+def winningPercentage(team_1, team_2, cleaner_obj, season_ls):
+    df = cleaner_obj.new_df
+    df = df.loc[df["season"].isin(season_ls)]
 
-# Replacing names of teams to their short names
-matches_2.replace(dic, inplace=True)
-
-def winningPercentage(team_1, team_2):
-    win_prcntage = (matches_2.winner.value_counts() / (matches_2.team1.value_counts() + matches_2.team2.value_counts())) * 100
+    win_prcntage = (df.winner.value_counts() / (df.team1.value_counts() + df.team2.value_counts())) * 100
+    
+    # if (win_prcntage.)
     
     win_prcntage = pd.DataFrame({
         'Team_Name': [team_1, team_2],
-        'Win %age': [win_prcntage[team_1], win_prcntage[team_2]]
+        'Win %age': [win_prcntage.get(team_1), win_prcntage.get(team_2)]
     })
-    
-    fig = px.pie(win_prcntage, names='Team_Name', values='Win %age', hole=0.3,
-                 color="Team_Name", color_discrete_map={team_1: "#e84393", team_2: "#434343"})
+
+    fig = px.pie(win_prcntage, names='Team_Name',
+                    values='Win %age', hole=0.3,
+                    color="Team_Name",
+                    color_discrete_map={team_1: "#e84393",
+                                        team_2: "#434343"})
     return fig
 
 
-def team_in_most_season(team_1, team_2):
-    series_1 = matches_2.loc[matches_2["team1"] == team_1].season.value_counts()
-    series_2 = matches_2.loc[matches_2["team1"] == team_2].season.value_counts()
-    
+def team_in_most_season(team_1, team_2, cleaner_obj, season_ls):
+    df = cleaner_obj.new_df
+    df = df.loc[df["season"].isin(season_ls)]
+
+    series_1 = df.loc[df["team1"] == team_1].season.value_counts()
+    series_2 = df.loc[df["team1"] == team_2].season.value_counts()
+
     df1 = pd.DataFrame({
         "Team_name": [team_1 for i in range(len(list(series_1.keys())))],
         "Season": list(series_1.keys()),
         "Match Count": list(dict(series_1).values()),
     })
-    
     df2 = pd.DataFrame({
         "Team_name": [team_2 for i in range(len(list(series_2.keys())))],
         "Season": list(series_2.keys()),
         "Match Count": list(dict(series_2).values()),
     })
-    
+
     df = pd.concat([df1, df2], ignore_index=True)
-    
-    fig = px.histogram(df, x='Season', y='Match Count', color="Team_name",
-                       barmode="group", color_discrete_map={team_1: "#55efc4", team_2: "#e84393"})
-    
+
+    fig = px.histogram(df, x='Season',
+                        y='Match Count', 
+                        color="Team_name",
+                        barmode="group", 
+                        color_discrete_map={team_1: "#55efc4",
+                                            team_2: "#e84393"})
+
     return fig
 
-def player_match_season(team_1, team_2):
-    series_1 = matches_2.loc[matches_2["team1"] == team_1].player_of_match.value_counts()[:5]
-    series_2 = matches_2.loc[matches_2["team1"] == team_2].player_of_match.value_counts()[:5]
-    
+def player_match_season(team_1, team_2, cleaner_obj, season_ls):
+    df = cleaner_obj.new_df
+    df = df.loc[df["season"].isin(season_ls)]
+
+    series_1 = df.loc[df["team1"] == team_1].player_of_match.value_counts()[:5]
+    series_2 = df.loc[df["team1"] == team_2].player_of_match.value_counts()[:5]
+
     df1 = pd.DataFrame({
         "Team_name": [team_1 for i in range(len(list(series_1.keys())))],
         "Player Name": list(series_1.keys()),
         "Match Count": list(dict(series_1).values()),
     })
-    
     df2 = pd.DataFrame({
         "Team_name": [team_2 for i in range(len(list(series_2.keys())))],
         "Player Name": list(series_2.keys()),
         "Match Count": list(dict(series_2).values()),
     })
-    
+
     df = pd.concat([df1, df2], ignore_index=True)
-    
-    fig = px.histogram(df, x='Player Name', y='Match Count', color="Team_name",
-                       barmode="group", color_discrete_map={team_1: "#55efc4", team_2: "#e84393"})
-    
+
+    fig = px.histogram(df, x='Player Name',
+                        y='Match Count', 
+                        color="Team_name",
+                        barmode="group", 
+                        color_discrete_map={team_1: "#55efc4",
+                                            team_2: "#e84393"})
+
     return fig
 
+def win_visu_by_toss(team_name, cleaner_obj, season_ls):
+    df = cleaner_obj.new_df
+    df = df.loc[df["season"].isin(season_ls)]
+    
+    # Getting all the data if `team_name` wins the toss.
+    field_data = df[(df["toss_winner"] == team_name) & (df["toss_decision"] == "field")]
+    bat_data = df[(df["toss_winner"] == team_name) & (df["toss_decision"] == "bat")]
 
-dic = {'Sunrisers Hyderabad': 'SRH', 'Kolkata Knight Riders': 'KKR',
-        'Royal Challengers Bangalore': 'RCB', 'Kings XI Punjab': 'KXIP',
-        'Mumbai Indians': 'MI', 'Chennai Super Kings': 'CSK',
-        'Rajasthan Royals': 'RR', 'Delhi Capitals': 'DC'
-        }
-matches.replace(dic, inplace=True)
-
-def win_visu_by_toss(team_name):
-    datas = matches[(matches['toss_winner']==team_name) & (matches['winner']==team_name)]
-    count = datas['toss_decision'].value_counts()
-    win_bat = count['bat']/(count['field']+count['bat'])*100
-    win_field = count['field']/(count['bat']+count['field'])*100
+    winning_field_data = len(field_data[field_data["winner"] == team_name])
+    winning_bat_data = len(bat_data[bat_data["winner"] == team_name])
+    
+    # Winning percentage based on fielding decision
+    win_field = (winning_field_data/ max(len(field_data), 1)) * 100
+    
+    # Winning percentage based on batting decision
+    win_bat = (winning_bat_data/ max(len(bat_data), 1)) * 100
+    
+    # Creating dataframe for plot
     data = [['Fielding', win_field], ['Batting', win_bat]]
     data = pd.DataFrame (data,columns=['Decision','Win_%age'])
     
